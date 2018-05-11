e83da8739fb466cfc4699b75ead40e87c7c0c082
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-11 18:58:30 +0200

    add class Movie Adapter for implementing Recycler View on the images

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.popularmoviesapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private MovieAdapter mMovieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_grid);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, GridLayoutManager.DEFAULT_SPAN_COUNT);
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mMovieAdapter = new MovieAdapter();
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+    }
+
+    //to implement
+    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            return new String[0];
+        }
     }
 }
--- app/src/main/java/com/example/popularmoviesapp/MovieAdapter.java
+++ app/src/main/java/com/example/popularmoviesapp/MovieAdapter.java
@@ -1,21 +1,59 @@
 package com.example.popularmoviesapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class MovieAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ImageViewHolder> {
+
+    private String[] mImageLinks;
+
+    public MovieAdapter() {
+
+    }
 
     class ImageViewHolder extends RecyclerView.ViewHolder {
-        ImageView listItemImageView;
+        public final ImageView listItemImageView;
 
-        public ImageViewHolder(View itemView) {
-            super(itemView);
-       //     listItemImageView = (ImageView) itemView.findViewById(R.id.);
-        }
-
-        void bind(int listIndex) {
-            // listItemImageView.setImage //load image from API
+        public ImageViewHolder(View view) {
+            super(view);
+            listItemImageView = itemView.findViewById(R.id.iv_movie_item);
         }
     }
+
+    @NonNull
+    @Override
+    public ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutId = R.layout.movie_grid_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+        View view = inflater.inflate(layoutId, parent, shouldAttachToParentImmediately);
+
+        return new ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ImageViewHolder holder, int position) {
+      //  holder.listItemImageView.setImage(mImageLinks[position])
+        //somethingsomething parsed from json
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mImageLinks == null)
+            return 0;
+        return mImageLinks.length;
+    }
+
+    public void setImageLinks(String[] imageLinks) {
+        mImageLinks = imageLinks;
+        //notify a change has been made?
+    }
+
+
 }
