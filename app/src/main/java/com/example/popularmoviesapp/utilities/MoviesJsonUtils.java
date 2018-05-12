846b6ba90dca6bc7e53e2b722349c980b1ea61ad
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-12 20:02:24 +0200

    start parsing JSON to for posters

---
+++ app/src/main/java/com/example/popularmoviesapp/utilities/MoviesJsonUtils.java
@@ -0,0 +1,39 @@
+package com.example.popularmoviesapp.utilities;
+
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public final class MoviesJsonUtils {
+
+    public static String[] getImageLinksFromJson(Context context, String moviesJsonStr) throws JSONException {
+        final String MDB_RESULTS = "results";
+        final String MDB_TITLE = "title";
+        final String MDB_POSTER = "poster_path";
+
+        String[] parsedMovieData;
+
+        JSONObject moviesJson = new JSONObject(moviesJsonStr);
+
+        JSONArray moviesArray = moviesJson.getJSONArray(MDB_RESULTS);
+
+        parsedMovieData = new String[moviesArray.length()];
+
+        for (int i = 0; i < moviesArray.length(); i++) {
+            String posterPath;
+            String title;
+
+            JSONObject movie = moviesArray.getJSONObject(i);
+
+            posterPath = movie.getString(MDB_POSTER);
+            title = movie.getString(MDB_TITLE);
+
+            parsedMovieData[i] = title + " = " + posterPath;
+        }
+
+        return parsedMovieData;
+    }
+
+}
