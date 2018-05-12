ef009753237856b4e0d90e4df115da6e3bf35544
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-12 17:46:24 +0200

    correct gridlayoutmanager declaration

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_grid);
-
-        GridLayoutManager layoutManager = new GridLayoutManager(this, GridLayoutManager.DEFAULT_SPAN_COUNT);
+        int numberOfCol = 2;
+        GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfCol);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setHasFixedSize(true);
 
