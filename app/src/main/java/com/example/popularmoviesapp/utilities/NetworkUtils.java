2550759d7ef3be3d12ca16d8296eeb3efa80cb39
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-12 19:47:20 +0200

    make http request with url to get response

--- app/src/main/java/com/example/popularmoviesapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/popularmoviesapp/utilities/NetworkUtils.java
@@ -4,8 +4,13 @@ import android.net.Uri;
 
 import com.example.popularmoviesapp.BuildConfig;
 
+import java.io.IOError;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 public final class NetworkUtils {
     private static final String MOVIES_BASE_URL = "https://api.themoviedb.org/3/movie/";
@@ -25,6 +30,25 @@ public final class NetworkUtils {
         }
         return url;
     };
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput)
+                return scanner.next();
+            else
+                return null;
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 
 
 }
