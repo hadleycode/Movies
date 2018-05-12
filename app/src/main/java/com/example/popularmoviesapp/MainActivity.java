6c6378d736ee8b87843121e3692ed03f4a34b5e4
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-12 20:15:17 +0200

    test http request to get JSON response from MDB

--- app/src/main/java/com/example/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/MainActivity.java
@@ -5,6 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.popularmoviesapp.utilities.NetworkUtils;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,28 +21,57 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private MovieAdapter mMovieAdapter;
 
+    private TextView testResponseTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_grid);
-        int numberOfCol = 2;
-        GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfCol);
-        mRecyclerView.setLayoutManager(layoutManager);
-        mRecyclerView.setHasFixedSize(true);
+        testResponseTextView = (TextView) findViewById(R.id.tv_test_response);
+        loadMovieData();
 
-        mMovieAdapter = new MovieAdapter();
-        mRecyclerView.setAdapter(mMovieAdapter);
+//        mRecyclerView = (RecyclerView) findViewById(R.id.rv_movie_grid);
+//        int numberOfCol = 2;
+//        GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfCol);
+//        mRecyclerView.setLayoutManager(layoutManager);
+//        mRecyclerView.setHasFixedSize(true);
+//
+//        mMovieAdapter = new MovieAdapter();
+//        mRecyclerView.setAdapter(mMovieAdapter);
 
     }
 
+    private void loadMovieData() {
+        //Can be changed to "top_rated"
+        String sortBy = "popular";
+
+        URL builtUrl = NetworkUtils.buildUrl(sortBy);
+        Log.d("Url", "The URL is : " + builtUrl);
+
+        new FetchMoviesTask().execute(builtUrl);
+    }
+
     //to implement
-    public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+    public class FetchMoviesTask extends AsyncTask<URL, Void, String> {
 
         @Override
-        protected String[] doInBackground(String... strings) {
-            return new String[0];
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+            String response = null;
+            try {
+                response = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")) {
+                testResponseTextView.setText(s);
+            }
         }
     }
 }
