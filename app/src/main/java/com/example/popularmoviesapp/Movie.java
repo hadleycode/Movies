ef31fab34ef95da8cd119603c5b7272464de32a6
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-14 15:59:02 +0200

    make movie object with parsed json data

--- app/src/main/java/com/example/popularmoviesapp/Movie.java
+++ app/src/main/java/com/example/popularmoviesapp/Movie.java
@@ -7,19 +7,19 @@ public class Movie implements Parcelable {
     private final int mVoteCount;
     private final int mId;
     private final boolean mVideo;
-    private final float mVoteAverage;
+    private final double mVoteAverage;
     private final String mTitle;
-    private final float mPopularity;
+    private final double mPopularity;
     private final String mPosterPath;
     private final String mOriginalLanguage;
     private final String mOriginalTitle;
-    private final int[] mGenreIds;
+ //   private final int[] mGenreIds;
     private final String mBackdropPath;
     private final boolean mAdult;
     private final String mOverview;
     private final String mReleaseDate;
 
-    public Movie(int mVoteCount, int mId, boolean mVideo, float mVoteAverage, String mTitle, float mPopularity, String mPosterPath, String mOriginalLanguage, String mOriginalTitle, int[] mGenreIds, String mBackdropPath, boolean mAdult, String mOverview, String mReleaseDate) {
+    public Movie(int mVoteCount, int mId, boolean mVideo, double mVoteAverage, String mTitle, double mPopularity, String mPosterPath, String mOriginalLanguage, String mOriginalTitle, String mBackdropPath, boolean mAdult, String mOverview, String mReleaseDate) {
         this.mVoteCount = mVoteCount;
         this.mId = mId;
         this.mVideo = mVideo;
@@ -29,7 +29,7 @@ public class Movie implements Parcelable {
         this.mPosterPath = mPosterPath;
         this.mOriginalLanguage = mOriginalLanguage;
         this.mOriginalTitle = mOriginalTitle;
-        this.mGenreIds = mGenreIds;
+   //     this.mGenreIds = mGenreIds;
         this.mBackdropPath = mBackdropPath;
         this.mAdult = mAdult;
         this.mOverview = mOverview;
@@ -41,13 +41,13 @@ public class Movie implements Parcelable {
         mVoteCount = in.readInt();
         mId = in.readInt();
         mVideo = in.readByte() != 0;
-        mVoteAverage = in.readFloat();
+        mVoteAverage = in.readDouble();
         mTitle = in.readString();
-        mPopularity = in.readFloat();
+        mPopularity = in.readDouble();
         mPosterPath = in.readString();
         mOriginalLanguage = in.readString();
         mOriginalTitle = in.readString();
-        mGenreIds = in.createIntArray();
+  //      mGenreIds = in.createIntArray();
         mBackdropPath = in.readString();
         mAdult = in.readByte() != 0;
         mOverview = in.readString();
@@ -66,62 +66,6 @@ public class Movie implements Parcelable {
         }
     };
 
-    public int getmVoteCount() {
-        return mVoteCount;
-    }
-
-    public int getmId() {
-        return mId;
-    }
-
-    public boolean ismVideo() {
-        return mVideo;
-    }
-
-    public float getmVoteAverage() {
-        return mVoteAverage;
-    }
-
-    public String getMtitle() {
-        return mTitle;
-    }
-
-    public float getmPopularity() {
-        return mPopularity;
-    }
-
-    public String getmPosterPath() {
-        return mPosterPath;
-    }
-
-    public String getmOriginalLanguage() {
-        return mOriginalLanguage;
-    }
-
-    public String getmOriginalTitle() {
-        return mOriginalTitle;
-    }
-
-    public int[] getmGenreIds() {
-        return mGenreIds;
-    }
-
-    public String getmBackdropPath() {
-        return mBackdropPath;
-    }
-
-    public boolean ismAdult() {
-        return mAdult;
-    }
-
-    public String getmOverview() {
-        return mOverview;
-    }
-
-    public String getmReleaseDate() {
-        return mReleaseDate;
-    }
-
 
     @Override
     public int describeContents() {
@@ -133,13 +77,13 @@ public class Movie implements Parcelable {
         dest.writeInt(mVoteCount);
         dest.writeInt(mId);
         dest.writeByte((byte) (mVideo ? 1 : 0));
-        dest.writeFloat(mVoteAverage);
+        dest.writeDouble(mVoteAverage);
         dest.writeString(mTitle);
-        dest.writeFloat(mPopularity);
+        dest.writeDouble(mPopularity);
         dest.writeString(mPosterPath);
         dest.writeString(mOriginalLanguage);
         dest.writeString(mOriginalTitle);
-        dest.writeIntArray(mGenreIds);
+     //   dest.writeIntArray(mGenreIds);
         dest.writeString(mBackdropPath);
         dest.writeByte((byte) (mAdult ? 1 : 0));
         dest.writeString(mOverview);
--- app/src/main/java/com/example/popularmoviesapp/utilities/MoviesJsonUtils.java
+++ app/src/main/java/com/example/popularmoviesapp/utilities/MoviesJsonUtils.java
@@ -2,18 +2,69 @@ package com.example.popularmoviesapp.utilities;
 
 import android.util.Log;
 
+import com.example.popularmoviesapp.Movie;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public final class MoviesJsonUtils {
 
+    final static String MDB_RESULTS = "results";
+    final static String MDB_TITLE = "title";
+    final static String MDB_POSTER = "poster_path";
+    final static String MDB_POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
+    final static String MDB_POSTER_SIZE = "w185";
+    final static String MDB_VOTE_COUNT = "vote_count";
+    final static String MDB_ID = "id";
+    final static String MDB_VIDEO = "video";
+    final static String MDB_VOTE_AVERAGE = "vote_average";
+    final static String MDB_POPULARITY = "popularity";
+    final static String MDB_ORIGINAL_LANGUAGE = "original_language";
+    final static String MDB_ORIGINAL_TITLE = "original_title";
+ //   final static String MDB_GENRE_IDS = "genre_ids";
+    final static String MDB_BACKDROP_PATH = "backdrop_path";
+    final static String MDB_ADULT = "adult";
+    final static String MDB_OVERVIEW = "overview";
+    final static String MD_RELEASE_DATE = "release_date";
+
+    public static Movie[] getMoviesFromJson(String moviesJsonStr) throws JSONException {
+        Movie[] parsedMovies;
+        int voteCount, id;
+    //    int[] genreIds;
+        boolean video, adult;
+        double voteAverage, popularity;
+        String title, posterPath, originalLanguage, originalTitle, backdropPath, overview, releaseDate;
+
+        JSONObject moviesJson = new JSONObject(moviesJsonStr);
+        JSONArray moviesArray = moviesJson.getJSONArray(MDB_RESULTS);
+        parsedMovies = new Movie[moviesArray.length()];
+
+        for (int i = 0; i < moviesArray.length(); i++) {
+            JSONObject movie = moviesArray.getJSONObject(i);
+            voteCount = movie.optInt(MDB_VOTE_COUNT);
+            id = movie.optInt(MDB_ID);
+            video = movie.optBoolean(MDB_VIDEO);
+            adult = movie.optBoolean(MDB_ADULT);
+            voteAverage = movie.optDouble(MDB_VOTE_AVERAGE);
+            popularity = movie.optDouble(MDB_POPULARITY);
+            title = movie.optString(MDB_TITLE);
+            posterPath = MDB_POSTER_BASE_URL + MDB_POSTER_SIZE + movie.optString(MDB_POSTER);
+            originalLanguage = movie.optString(MDB_ORIGINAL_LANGUAGE);
+            originalTitle = movie.optString(MDB_ORIGINAL_TITLE);
+            backdropPath = movie.optString(MDB_BACKDROP_PATH);
+            overview = movie.optString(MDB_OVERVIEW);
+            releaseDate = movie.optString(MD_RELEASE_DATE);
+
+            parsedMovies[i] = new Movie(voteCount, id, video, voteAverage, title, popularity, posterPath, originalLanguage, title, backdropPath, adult, overview, releaseDate);
+        }
+
+        return parsedMovies;
+
+
+    }
+
     public static String[] getImageLinksFromJson(String moviesJsonStr) throws JSONException {
-        final String MDB_RESULTS = "results";
-//        final String MDB_TITLE = "title";
-        final String MDB_POSTER = "poster_path";
-        final String MDB_POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
-        final String MDB_POSTER_SIZE = "w185";
 
         String[] parsedMovieData;
 
