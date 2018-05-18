8ba25048a5b64320317c534d614ecdcf1f5635eb
hadleycode <hadleydaniel@gmail.com>
2018-05-18 16:05:28 +0200

    add more info to the detail layout

--- app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
@@ -1,11 +1,13 @@
 package com.example.popularmoviesapp;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,11 +17,26 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         TextView movieTitleTextView = findViewById(R.id.tv_movie_title);
+        TextView originalTitleTextView = findViewById(R.id.tv_original_title);
+        TextView originalLanguageTextView = findViewById(R.id.tv_original_language);
+        TextView overviewTextView = findViewById(R.id.tv_overview);
+        TextView releaseDateTextView = findViewById(R.id.tv_release_date);
+        ImageView posterImageView = findViewById(R.id.iv_poster);
 
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra("movie");
 
-        movieTitleTextView.setText(movie.getmTitle());
+        movieTitleTextView.setText("Title : " + movie.getmTitle());
+        originalTitleTextView.setText("Original Title : " + movie.getmOriginalTitle());
+        originalLanguageTextView.setText("Original Language : " + movie.getmOriginalLanguage());
+        overviewTextView.setText("Overview : " + movie.getmOverview());
+        releaseDateTextView.setText("Released on : " + movie.getmReleaseDate());
+
+        Context context = DetailActivity.this;
+        String imageLink = movie.getmPosterPath();
+
+        Picasso.with(context).load(imageLink).into(posterImageView);
+
 
     }
 }
