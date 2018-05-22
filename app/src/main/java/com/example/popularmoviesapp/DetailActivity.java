e69124ebdd6825a55b09b8fbb6c3293dd09b0e6b
Daniel Hadley <hadleydaniel@gmail.com>
2018-05-22 15:02:14 +0200

    add rating

--- app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
+++ app/src/main/java/com/example/popularmoviesapp/DetailActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -23,19 +25,28 @@ public class DetailActivity extends AppCompatActivity {
         TextView releaseDateTextView = findViewById(R.id.tv_release_date);
         ImageView posterImageView = findViewById(R.id.iv_poster);
         ImageView splashPosterImageView = findViewById(R.id.iv_poster_splash);
+        RatingBar movieScoreRatingBar = findViewById(R.id.rb_movie_score);
 
         Intent intent = getIntent();
         Movie movie = intent.getParcelableExtra("movie");
 
-        movieTitleTextView.setText("Title : " + movie.getmTitle());
-        originalTitleTextView.setText("Original Title : " + movie.getmOriginalTitle());
+        movieTitleTextView.setText(movie.getmTitle());
+        if (movie.getmTitle().equals(movie.getmOriginalTitle())) {
+            originalTitleTextView.setVisibility(View.GONE);
+        } else
+            originalTitleTextView.setText(movie.getmOriginalTitle());
+
         overviewTextView.setText("Overview : " + movie.getmOverview());
-        releaseDateTextView.setText("Released on : " + movie.getmReleaseDate());
+        releaseDateTextView.setText("Released : " + movie.getmReleaseDate());
 
         Context context = DetailActivity.this;
         String imageLink = movie.getmPosterPath();
         String splashImageLink = movie.getmBackdropPath();
         Log.d("Splash Image", splashImageLink);
+
+        float average = (float) movie.getmVoteAverage();
+        movieScoreRatingBar.setRating(average);
+
 
         Picasso.with(context).load(imageLink).into(posterImageView);
         Picasso.with(context).load(splashImageLink).into(splashPosterImageView);
